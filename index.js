46d235f03936434c6d1a9703ff778aa1e0ac5e15
Anthony Lockard <anttontb@yahoo.com>
2017-09-09 05:08:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ function positionToInteger(p) {
 }
 
 function start() {
-  window.addEventListener('keydown', moveDodger)
+  document.addEventListener('keydown', moveDodger)
 
   START.style.display = 'none'
 
